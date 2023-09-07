52dac174853bb112bf23cbfc9fabf71aad8d83b1
Brendan Kumasaka <bhk22@hawaii.edu>
2023-09-07 13:57:14 -1000

    Completed up until Do Your Own

---
+++ secondViewController.swift
@@ -0,0 +1,27 @@
+//
+//  secondViewController.swift
+//  HackwichTwo_9.5.23
+//
+//  Created by Brendan Kumasaka on 9/7/23.
+//
+
+import UIKit
+
+class secondViewController:
+    UIViewController  {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Do any additional setup after loading the view.
+    }
+    
+    @IBAction func
+    changeColorButtonPressed(_ sender: Any)
+    {
+        //When the button is pressed the background color of the view is set to blue
+        self.view.backgroundColor = UIColor.blue
+        
+    }
+    
+    }
